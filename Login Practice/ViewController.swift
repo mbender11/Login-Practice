2b9540173a403139506623583f03a26b9656f62e
Mason Bender <bender_mason@yahoo.com>
2018-02-26 11:59:16 -0700

    Saved user's name using core data and display a greeting using their name when they open the app.

--- Login Practice/ViewController.swift
+++ Login Practice/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func login(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        newUser.setValue(textField.text, forKey: "name")
+        
+        do {
+            try context.save()
+            textField.alpha = 0
+            loginButton.alpha = 0
+            label.alpha = 1
+            label.text = "Hello, " + textField.text!
+        } catch {
+            print("Error while saving")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            for result in results as! [NSManagedObject] {
+                if let username = result.value(forKey: "name") as? String {
+                    textField.alpha = 0
+                    loginButton.alpha = 0
+                    label.alpha = 1
+                    label.text = "Hello, \(username)!"
+                }
+            }
+        } catch {
+            print("something went wrong :(")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
