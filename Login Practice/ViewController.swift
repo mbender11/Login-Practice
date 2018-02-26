8155bdabcbe3822ff882e277f61a5448af4a5341
Mason Bender <bender_mason@yahoo.com>
2018-02-26 16:10:54 -0700

    Added ability to update username instead of just logging out, and updated UI to support it

--- Login Practice/ViewController.swift
+++ Login Practice/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var logoutButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
     @IBAction func login(_ sender: Any) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -23,12 +24,38 @@ class ViewController: UIViewController {
         
         do {
             try context.save()
-            textField.alpha = 0
-            loginButton.alpha = 0
+            loginButton.setTitle("Update username", for: [])
+            logoutButton.alpha = 1
             label.alpha = 1
             label.text = "Hello, " + textField.text!
         } catch {
             print("Error while saving")
+        }
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results {
+                    context.delete(result as! NSManagedObject)
+                }
+                do {
+                    try context.save()
+                } catch {
+                    print("Individual deletion failed")
+                }
+            }
+            loginButton.setTitle("Login", for: [])
+            label.alpha = 0
+            logoutButton.alpha = 0
+            
+        } catch {
+            print("Deletion failed")
         }
     }
     
@@ -46,8 +73,8 @@ class ViewController: UIViewController {
             let results = try context.fetch(request)
             for result in results as! [NSManagedObject] {
                 if let username = result.value(forKey: "name") as? String {
-                    textField.alpha = 0
-                    loginButton.alpha = 0
+                    loginButton.setTitle("Update username", for: [])
+                    logoutButton.alpha = 1
                     label.alpha = 1
                     label.text = "Hello, \(username)!"
                 }
